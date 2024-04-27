f1a8b473395eafed9ec5d5ec5bcb5bfd92a6f04f
DesignSupply <ogawa@designsupply-web.com>
2024-04-27 10:33:09 +0900

    [add] ベースのファイル群追加

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: import.meta.env.MODE === 'production' ? 'https://example.com' : 'http://localhost:2000',
+  base: '/',
+  trailingSlash: 'ignore',
+  output: 'static',
+  root: '.',
+  srcDir: './src',
+  publicDir: './public',
+  outDir: './dist',
+  compressHTML: false,
+  build: {
+    format: 'preserve'
+  },
+  server: {
+    port: 2000,
+    open: true
+  }
+});
---
+++ siteData.mjs
@@ -0,0 +1,9 @@
+export default {
+  home: {
+    title: 'サイトタイトル',
+    description: 'サイトタイトルの説明文です',
+    ogpImage: '/assets/images/ogp_default.png',
+    type: 'website',
+    path: '/',
+  }
+}
