ea13c220a453f9e73d045f5bd1450031e41b7105
DesignSupply <ogawa@designsupply-web.com>
2024-05-20 07:38:16 +0900

    [add] Stylelint導入

---
+++ stylelint.config.js
@@ -0,0 +1,25 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: [
+    'stylelint-config-html',
+    'stylelint-config-standard-scss'
+  ],
+  plugins: [
+    'stylelint-scss',
+    'stylelint-prettier',
+  ],
+  overrides: [
+    {
+      files: [
+        '*.{html,astro}', 
+        '**/*.{html,astro}'
+      ],
+      customSyntax: 'postcss-html'
+    }
+  ],
+  rules: {
+    'prettier/prettier': true,
+    'at-rule-no-unknown': [ true, { 'ignoreAtRules': ['tailwind'] } ],
+    'scss/at-rule-no-unknown': [ true, { 'ignoreAtRules': ['tailwind'] } ]
+  }
+};
