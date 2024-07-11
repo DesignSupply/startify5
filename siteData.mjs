b3b139b9b22c5afd95a818346b3994219aef86d1
DesignSupply <ogawa@designsupply-web.com>
2024-07-11 20:18:49 +0900

    [fix] 固定値での記述箇所の修正

--- siteData.mjs
+++ siteData.mjs
@@ -7,17 +7,17 @@ export default {
     path: '/'
   },
   example: {
-    title: 'テストページ',
-    description: 'テストページの説明文です',
+    title: '下層サンプルページ',
+    description: '下層サンプルページの説明文です',
     ogpImage: '/assets/images/ogp_default.png',
     type: 'article',
     path: '/example.html'
   },
   posts: {
     title: '投稿一覧',
-    description: '投稿一覧の説明文です',
+    description: '投稿一覧ページの説明文です',
     ogpImage: '/assets/images/ogp_default.png',
     type: 'article',
-    path: '/posts'
+    path: '/posts/'
   }
 };
